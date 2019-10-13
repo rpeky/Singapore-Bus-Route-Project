97e1882c335b094cb4fe1c2f9c2f02ff76c50381
Ryan <35019166+rpeky@users.noreply.github.com>
2019-10-13 20:03:57 +0800

    converted the test into a function need to figure out what the busstops api attribute returns and how to specify data

--- running/running.py
+++ running/running.py
@@ -5,17 +5,15 @@ from urllib.parse import urlparse
 
 import httplib2 as http 
 
-if __name__=="__main__":
-    headers = {'AccountKey' : 'V3Vgr4XCSiCOCLGwxt6QbQ==',
-               'accept' : 'application/json'  
-               }
+headers = {'AccountKey' : 'V3Vgr4XCSiCOCLGwxt6QbQ==',
+           'accept' : 'application/json'  
+          }
 
-
+def busstopcodedata(stopno):
     uri = 'http://datamall2.mytransport.sg/'
-    path = 'ltaodataservice/BusServices' 
-    x = '123455' #busstop number
-
-    target = urlparse('http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=' + x)
+    path = 'ltaodataservice/BusServices='
+    
+    target = urlparse(uri+path+stopno)
     target.geturl()
     method = 'GET'
     body = ''      
@@ -32,9 +30,54 @@ if __name__=="__main__":
     jsonObj = json.loads(content)
     jsonprintingxd = json.dumps(jsonObj, sort_keys=True, indent = 4)
     print (jsonprintingxd)
-
+   
     with open("bus_routes.json","w") as outfile:
         json.dump(jsonObj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+def busstopsget(busn):
+    #http://datamall2.mytransport.sg/ltaodataservice/BusStops
+    uri = 'http://datamall2.mytransport.sg/'
+    path = 'ltaodataservice/BusStops='
+    
+    target = urlparse(uri+path+busn)
+    target.geturl()
+    method = 'GET'
+    body = ''      
+    
+    h = http.Http()
+
+    response, content = h.request (
+        target.geturl(),
+        method,
+        body,
+        headers
+        )
+
+    jsonObj = json.loads(content)
+    jsonprintingxd = json.dumps(jsonObj, sort_keys=True, indent = 4)
+    print (jsonprintingxd)
+   
+    with open("bus_routes.json","w") as outfile:
+        json.dump(jsonObj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+if __name__=="__main__":
+
+    x = '01239' #busstop number
+    printxd = busstopcodedata(x)
+    print(printxd)
+
+   #y = '12'
+   #printingxd = busstopsget(y)
+   #print(printingxd)
+
+
+
+     
+    
+
+    
 
 
 
