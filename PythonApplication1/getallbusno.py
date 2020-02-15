c1d30ae4544b0221636b1fa8aa0bf7a13b900908
Ryan <35019166+rpeky@users.noreply.github.com>
2020-02-15 21:00:40 +0800

    thanks to brandon, busroute data has been logged :)

--- PythonApplication1/getallbusno.py
+++ PythonApplication1/getallbusno.py
@@ -1,35 +1,3 @@
-#import requests
-
-
-#headers = {'AccountKey' : '4BXSLAQ5T+C4NJ6TA9/qjA==',
-#           'accept' : 'application/json'  
-#          }
-
-
-
-#def busroute():
-#    url = 'http://datamall2.mytransport.sg/ltaodataservice/BusRoutes'
-#    jsonobj = requests.get(url).json()
-#    with open("busroutejsonfile123123.json","w") as outfile:
-#        json.dump(jsonobj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
-
-#    #if(skips > 0):
-#    #    jsonobj = requests.get(url+urladditions).json()
-#    #    with open("busroutejsonfile.json","w") as outfile:
-#    #        json.dump(jsonobj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
-
-
-
-
-
-#if __name__ == "__main__":
-#    #for number in range(50):
-#    busroute()
-#    print("done :)") 
-
-
-
-
 import json
 import urllib
 import urllib.request
@@ -44,7 +12,7 @@ headers = {'AccountKey' : '4BXSLAQ5T+C4NJ6TA9/qjA==',
 ## $skip=x function for api call is broken ==> see plan b
 def busroute(skips):
     if skips == 0:
-        target = urlparse('http://datamall2.mytransport.sg/ltaodataservice/BusRoutes$skip=500') 
+        target = urlparse('http://datamall2.mytransport.sg/ltaodataservice/BusRoutes?') 
         target.geturl()
         method = 'GET'
         body = ''
@@ -61,12 +29,14 @@ def busroute(skips):
         jsonobj = json.loads(content)
         #print(json.dumps(content, sort_keys=True, indent=4))
 
-        with open("bus_stop_no_info123123.json","w") as outfile:
+        with open("bus_route_info_0.json","w") as outfile:
             json.dump(jsonobj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+        print("done")
 
     else:
         skippos = str(skips*500)
-        target = urlparse('http://datamall2.mytransport.sg/ltaodataservice/BusRoutes$skip='+skippos)
+        target = urlparse('http://datamall2.mytransport.sg/ltaodataservice/BusRoutes?$skip='+skippos)
         target.geturl()
         method = 'GET'
         body = ''
@@ -83,14 +53,17 @@ def busroute(skips):
         jsonobj = json.loads(content)
        #print(json.dumps(jsonObj, sort_keys=True, indent=4))
         json.loads(content)
-        with open("bus_stop_no_info"+skippos+".json","w") as outfile:
+        with open("bus_route_info_"+skippos+".json","w") as outfile:
             json.dump(jsonobj, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+        print("done")
 
 
 
 
 
 if __name__ == "__main__":
+    for number in range(100):
+        busroute(number)
 
-    busroute(0)
     print("done :)") 
